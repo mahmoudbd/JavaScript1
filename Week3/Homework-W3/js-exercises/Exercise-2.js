04f3592c0ef902126e5544c6c53ffb3449148994
Mahmoud Badran <56569128+mahmoudbd@users.noreply.github.com>
2019-12-14 17:38:36 +0100

    Delete Exercise-2.js

--- Week3/Homework-W3/js-exercises/Exercise-2.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function calculateDogAge(ageDog) {
-	return `Your doggie is ${ageDog * 7} years old in dog years!`;
-}
-console.log(calculateDogAge(6));
-console.log(calculateDogAge(2));
-console.log(calculateDogAge(5));
-console.log(calculateDogAge(3));
